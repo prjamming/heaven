b625b11f29ac7d0a4e72ead6c4c1f3ee9ef95eb3
prjamming <124306301+prjamming@users.noreply.github.com>
2024-02-22 13:41:55 +0000

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ const config = {
   users: { // You can add multiple users by doing username: 'password'. 
     prjamming: '0v3rth3t0p!',
     slthgmr333: 'Wyatt135',
+    wackyman: 'hubx2'
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
