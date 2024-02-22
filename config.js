02b4ff7702fc4bbfcb085fcc456621d6d39d4f1d
prjamming <124306301+prjamming@users.noreply.github.com>
2024-02-22 13:50:45 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const config = {
   challenge: true, // Set to true if you want to enable password protection.
-  users: { // You can add multiple users by doing username: 'password'. 
-    prjamming: '0v3rth3t0p!',
+  users: { // You can add multiple users by doing username: 'password'.
     slthgmr333: 'Wyatt135',
     wackyman: 'hubx2'
   },
